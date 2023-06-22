f2f9b40cedc943303b3cf543c4cc731664841bc3
Nayan Joshi <50129883+Nayan-joshi07@users.noreply.github.com>
2023-06-22 16:45:47 +0530

    Bug fix

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"onCreate() is called" , Toast.LENGTH_LONG).show();
         welcome_text = findViewById(R.id.welcome_textView);
         result_text = findViewById(R.id.result_textView);
         btn = findViewById(R.id.btn);
@@ -28,6 +30,35 @@ public class MainActivity extends AppCompatActivity {
                 result_text.setText(""+convertToPounds(kilos_entered));
             }
         });
+    }
+
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        Toast.makeText(this,"onStart() is called" , Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this,"onResume() is called" , Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected  void onRestart(){
+        super.onRestart();
+        Toast.makeText(this,"onRestart() is called" , Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        Toast.makeText(this,"onPause() is called" , Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected  void onStop(){
+        super.onStop();
+        Toast.makeText(this,"onStop() is called" , Toast.LENGTH_LONG);
     }
 
     public double convertToPounds(double kilos){
